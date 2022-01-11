96f1251c2e89ba4e5cc4c3afc379887596f66cf0
gzh <18255106634@163.com>
2022-01-11 17:24:01 +0800

    delete sourcemap dev环境不删除

--- packages/deleteSourcemap/lib/index.ts
+++ packages/deleteSourcemap/lib/index.ts
@@ -5,6 +5,13 @@ import { Compiler, Stats } from 'webpack';
 export default class DeleteSourcemap {
     apply(compile: Compiler) {
         compile.hooks.done.tapAsync('SentryPlugin', (stats: Stats, cb: () => void) => {
+            const isDevServer = process.argv.find((v) => v.includes('webpack-dev-server'));
+            // dev 直接 return
+            if (isDevServer) {
+                console.info('dev 环境不做删除sourcempa处理');
+                return;
+            }
+
             const regexp = /.+\.js.map$/;
             const assets = stats.compilation.assets;
 
